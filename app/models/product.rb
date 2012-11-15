ae654ab62327d99ff45578e01cd510cd5bf46afd
menuitem <mishmash111@gmail.com>
2012-11-15 20:54:36 +0000

    We added some validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :location, :name, :price, :user
+  validates :name, :description, :price, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 1.00}
+  validates :image_url, allow_blank: true, format: {
+  	with: /\.(png|gif|jpg)/i,
+  	message: 'must be a URL for GIF, JPG or PNG image'}
 end
