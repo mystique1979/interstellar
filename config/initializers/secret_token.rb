1d8aa8a4cf6a3aad4df49028df37d21f76331b4f
menuitem <mishmash111@gmail.com>
2012-11-15 13:28:18 +0000

    Interstellar project, just first commit after creating raw project with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Interstellar::Application.config.secret_token = '87c48beaee65ba3edda15aa971119256622b0a80dc1e32605fb12a6f9216cd6035b173b5f131b391473c3ff0eb6aa8ea3cd2fe80a135f488eb0ccf6cf9665d0b'
