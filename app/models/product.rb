b44aa4e553e2fd2775aa85dac6b94c56a5ceb6fb
menuitem <mishmash111@gmail.com>
2012-11-16 17:57:24 +0000

    Creating star shop cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,16 @@ class Product < ActiveRecord::Base
   validates :image_url, allow_blank: true, format: {
   	with: /\.(png|gif|jpg)/i,
   	message: 'must be a URL for GIF, JPG or PNG image'}
+  has_many :line_items
+  before_destroy :ensure_not_reference_by_any_line_item
+
+  private
+  	def ensure_not_reference_by_any_line_item
+  		if line_items.empty?
+  			return true
+  		else
+  			errors.add(:base, 'Line item present')
+  			return false
+  		end
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Interstellar::Application.routes.draw do
+  resources :line_items
+
+  resources :carts
+
   get "store/index"
 
   resources :products
